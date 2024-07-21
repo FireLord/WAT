16b00a50cf65672c9919c68241855c76827ee3ce
Yadvendra Sharma <yadvendras20@gmail.com>
2024-07-21 14:21:50 +0530

    added Dockerfile

--- dist/controller/message.controller.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from "express";
-export declare const sentMessage: (req: Request, res: Response) => Promise<void>;
-export declare const getAllMessages: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
