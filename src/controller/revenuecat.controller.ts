7dc933328d1bf08c66ee6f055152f2a156a946d5
Yadvendra Sharma <yadvendras20@gmail.com>
2024-09-02 17:21:23 +0530

    webhook test

--- src/controller/revenuecat.controller.ts
+++ src/controller/revenuecat.controller.ts
@@ -3,9 +3,13 @@ import { Request, Response } from "express";
 
 export const revenueCatHook = async (req: Request, res: Response) => {
     try {
+        console.log(new Date().toString());
         console.log("body:",req.body);
         console.log("querry params:",req.query);
         console.log("headers:",req.headers);
+        console.log("...");
+        console.log("...");
+
 
         
     } catch (error) {
